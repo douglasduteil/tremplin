fdc480dc1d114ae89eb00489cdd4cf017c516570
Thomas Glatt <thomas.glatt@eutelmed.com>
2019-03-05 16:02:13 +0100

    chore: remove next-sass

--- packages/app/next.config.js
+++ packages/app/next.config.js
@@ -1,14 +1,9 @@
 const withPlugins = require('next-compose-plugins');
 
 const withTypescript = require('@zeit/next-typescript');
-const withSass = require('@zeit/next-sass');
-const withCSS = require('@zeit/next-css');
 
 module.exports = withPlugins(
-	[
-		[withTypescript],
-		[withCSS],
-		[withSass],
-	]
+  [
+    [withTypescript]
+  ]
 );
-
