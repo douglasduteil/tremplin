700541a9335589d3dd2efcccf2852220c598fbf9
Thomas Glatt <thomas.glatt@eutelmed.com>
2019-03-19 17:40:36 +0100

    feat: use react hook in offers page

--- packages/app/pages/offers.tsx
+++ packages/app/pages/offers.tsx
@@ -2,38 +2,33 @@ import { JobOfferListItem } from "@components/jobOffers";
 import { PageLayout } from "@components/layout";
 import { JobOffer } from "@domain";
 import { jobOfferRepository } from '@repository';
-import React, { Component } from "react";
+import React, { useEffect, useState } from "react";
 import { Subscription } from "rxjs";
 
-export default class JobOffersPage extends Component<{}, { jobOffers: JobOffer[] }> {
+export default () => {
+  const subscription = new Subscription();
 
-  private subscription!: Subscription;
+  const [jobOffers, setJobOffers] = useState<JobOffer[]>([]);
 
-  public constructor(props: {}) {
-    super(props);
-    this.state = { jobOffers: [] };
-  }
+  useEffect(() => {
+    subscription.add(
+      jobOfferRepository().findAll().subscribe(res => { setJobOffers(res); })
+    );
+    return () => {
+      subscription.unsubscribe();
+    };
+  });
 
-  public componentDidMount() {
-    this.subscription = jobOfferRepository().findAll().subscribe(jobOffers => this.setState({ jobOffers }));
-  }
-
-  public componentWillUnmount() {
-    this.subscription.unsubscribe();
-  }
-
-  public render() {
-    return (
-      <PageLayout>
-        <h1>Offres</h1>
-        <ul>
-          {this.state.jobOffers.map(jobOffer => <JobOfferListItem key={jobOffer.id} jobOffer={jobOffer}></JobOfferListItem>)}
-        </ul>
-      </PageLayout>
-    )
-  }
-
+  return (
+    <PageLayout>
+      <h1>Offres</h1>
+      <ul>
+        {jobOffers.map(jobOffer => <JobOfferListItem key={jobOffer.id} jobOffer={jobOffer}></JobOfferListItem>)}
+      </ul>
+    </PageLayout>
+  )
 }
+
 
 
 
